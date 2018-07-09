734c97b4b6f94f68eccf6ea8d3e32458c593d11e
Kristaps Berzinch <kristaps@gatech.edu>
2018-07-09 19:48:51 -0400

    Code Climate tweaks

--- onken/public/tests.py
+++ onken/public/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+
 class DummyTest(TestCase):
     def test_dummy_test(self):
         self.assertIs(True, True)
--- onken/settings/baseline.py
+++ onken/settings/baseline.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,7 +83,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=None)
 DATABASES['default'].update(db_from_env)
 
