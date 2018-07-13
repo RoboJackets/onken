2608d4e5bfd118b451c02b50dbeaf9d8e0f34aa7
Kristaps Berzinch <kristaps@gatech.edu>
2018-07-12 23:57:58 -0400

    Manually specify urlconf(???)

--- onken/workspace/tests.py
+++ onken/workspace/tests.py
@@ -1,6 +1,7 @@
 from django_tenants.test.cases import TenantTestCase
 from django_tenants.test.client import TenantClient
 from django.shortcuts import reverse
+from django.conf import settings
 
 
 class IndexTest(TenantTestCase):
@@ -9,5 +10,5 @@ class IndexTest(TenantTestCase):
         self.c = TenantClient(self.tenant)
 
     def test_index(self):
-        response = self.c.get(reverse('workspace_index'))
+        response = self.c.get(reverse('workspace_index', urlconf=settings.ROOT_URLCONF))
         self.assertContains(response, "This is the workspace app.", status_code=200)
