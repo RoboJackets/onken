452026fae436f935681e9e7e00b5e77cf93377d5
Ross Lindsay <ross@rosslindsay.com>
2018-07-24 21:17:25 -0400

    Testing different serializers

--- onken/public/serializers.py
+++ onken/public/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
