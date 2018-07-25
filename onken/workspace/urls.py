6d78859b27bf5cb941176922ddcee6ad4125ed6f
Ross Lindsay <ross@rosslindsay.com>
2018-07-24 21:52:11 -0400

    Add DRF to the workspace app

--- onken/workspace/urls.py
+++ onken/workspace/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 import onken.workspace.views
+from django.conf.urls import url, include
+from rest_framework import routers
+import onken.workspace.api_views
+
+router = routers.DefaultRouter()
+router.register(r'users', onken.workspace.api_views.UserViewSet)
+router.register(r'groups', onken.workspace.api_views.GroupViewSet)
 
 urlpatterns = [
-    path('', onken.workspace.views.index, name="workspace_index")
+    path('', onken.workspace.views.index, name="workspace_index"),
+    url(r'^api/', include(router.urls)),
 ]
