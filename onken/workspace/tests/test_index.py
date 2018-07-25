73618874ae8fc73ae27b17a2c8fbce28a188852a
Ross Lindsay <ross@rosslindsay.com>
2018-07-24 22:14:37 -0400

    Update test to reflect CAS auth

--- onken/workspace/tests/test_index.py
+++ onken/workspace/tests/test_index.py
@@ -1,8 +1,14 @@
 from django.shortcuts import reverse
 from onken.workspace.test import WorkspaceTestCase
+from django.contrib.auth.models import User
 
 
 class IndexTest(WorkspaceTestCase):
     def test_index(self):
+        user = User(username='gburdell3', first_name='George')
+        user.save()
+
+        self.client.force_login(user)
+
         response = self.client.get(reverse('workspace_index'))
         self.assertContains(response, "This is the workspace app.", status_code=200)
