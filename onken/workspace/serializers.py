2dc727349ac18812b7bab08ce6417697d4067901
Ross Lindsay <ross@rosslindsay.com>
2018-07-29 17:36:00 -0400

    style

--- onken/workspace/serializers.py
+++ onken/workspace/serializers.py
@@ -14,6 +14,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ('url', 'name')
 
+
 class VendorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Vendor
