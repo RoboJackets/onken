edab0037328b402e2852b8c574b384a009305348
Ross Lindsay <ross@rosslindsay.com>
2018-07-29 20:27:25 -0400

    Remove old comment

--- onken/settings/development.py
+++ onken/settings/development.py
@@ -185,8 +185,6 @@ DATADOG_TRACE = {
 
 # DRF configuration
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissions'
     ],
